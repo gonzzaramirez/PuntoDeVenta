08e5fdb06bc578cdb015f14b4b3270bdeacdc56e
gonzzaramirez <gonza37754@gmail.com>
2023-08-31 18:13:07 -0300

    Label

--- posMate/Form1.Designer.cs
+++ posMate/Form1.Designer.cs
@@ -29,32 +29,45 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(163, 96);
+            this.button1.Location = new System.Drawing.Point(126, 93);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(477, 225);
             this.button1.TabIndex = 0;
             this.button1.Text = "Esto no es un boton";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(347, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HOLAAAAAAAAAA";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "PosMate";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
